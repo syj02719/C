9be7e81b92cd0e9b2d52554e9a80f07318c9e623
YooJeong <91018849+syj02719@users.noreply.github.com>
2021-11-16 23:01:22 +0900

    Add files via upload

---
+++ hw6.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int arr1[6] = { 1, 2, 3, 4, 5, 6 };
+   int arr2[6] = { 7, 8, 9, 10, 11, 12 };
+   int* fptr = &arr1[0];
+   int* bptr = &arr2[0];
+   int i, temp;
+
+   printf("arr1: ");
+   for (i = 0; i < 6; i++)
+   {
+      printf("%d ", arr1[i]);
+   }
+   printf("\n");
+    
+   printf("arr2:");
+   for (i = 0; i < 6; i++)
+   {
+      printf("%d ", arr2[i]);
+   }
+
+   printf("\nafter swap\n");
+
+   for (i = 0; i < 6; i++)
+   {
+      temp = * (fptr + i);
+      * (fptr + i) = * (bptr + i);
+      * (bptr + i) = temp;
+   }
+
+   printf("arr1: ");
+   for (i = 0; i < 6; i++)
+   {
+      printf("%d ", arr1[i]);
+   }
+   printf("\n");
+   printf("arr2: ");
+   for (i = 0; i < 6; i++)
+   {
+      printf("%d ", arr2[i]);
+   }
+}
