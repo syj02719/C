39c5d57d5419fa0e3ea2ec9cad9fc4b8866ba2bf
YooJeong <91018849+syj02719@users.noreply.github.com>
2021-11-30 22:27:24 +0900

    Add files via upload

---
+++ hw8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int convCase(int ch)
+{
+    const int diff = 'a' - 'A';
+    if (ch >= 'A' && ch <= 'Z')
+        return ch + diff ;
+    else if (ch >= 'a' && ch <= 'z')
+        return ch - diff ;
+    else
+        return ch ;
+}
+
+
+int main(void)
+{
+    char str[50] ;
+    int i = 0 ;
+    printf("input > ") ;
+    fgets(str, sizeof(str), stdin) ;
+    for (i = 0 ; i < (int) strlen(str) ; i++)
+    {
+        str[i] = convCase(str[i]) ;
+    }
+    printf("output > ") ;
+    printf("%s", str) ;
+    return 0 ;
+}
